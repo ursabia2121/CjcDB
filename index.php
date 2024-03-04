6af66ca2d168a248d5e771f2269af3a4d4ee3a5f
ursabia2121 <ursabia2121@gmail.com>
2024-03-04 15:59:39 +0800

    HALOO

--- index.php
+++ index.php
@@ -1,4 +1,71 @@
-<php
-    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login.html" method="post" name="register" onsubmit="return validateForm()">
+    <div class="loader">
+        <span class="loader-text">First Name</span>
+    </div>
+    <input type="text" id="first_name" name="first_name" required><br>
 
-php>
+    <div class="loader">
+        <span class="loader-text">Last Name</span>
+    </div>
+    <input type="text" id="last_name" name="last_name" required><br>
+
+    <div class="loader">
+        <span class="loader-text">Password</span>
+    </div>
+    <input type="password" id="password" name="password" required><br>
+
+    <div class="loader">
+        <span class="loader-text">Email</span>
+    </div>
+    <input type="email" id="email" name="email" required><br>
+
+    <div class="loader">
+        <span class="loader-text">Home address</span>
+    </div>
+    <input type="text" id="home_address" name="home_address" required><br>
+    
+    <div class="loader">
+        <span class="loader-text">Home address extra</span>
+    </div>
+    <input type="text" id="home_address_extra" name="home_address_extra" required><br>
+
+    <div class="loader">
+        <span class="loader-text">Zip code</span>
+    </div>
+    <input type="text" id="home_zip" name="home_zip" required><br>
+
+    <div class="loader">
+        <span class="loader-text">Username</span>
+    </div>
+    <input type="text" id="home_city" name="home_city" required><br>
+
+    <div class="loader">
+        <span class="loader-text">Username</span>
+    </div>
+    <input type="text" id="username" name="username" required><br>
+
+    <div class="loader">
+        <span class="loader-text">Username</span>
+    </div>
+    <input type="text" id="username" name="username" required><br>
+
+    <div class="loader">
+        <span class="loader-text">Username</span>
+    </div>
+    <input type="text" id="username" name="username" required><br>
+
+    <div class="loader">
+        <span class="loader-text">Username</span>
+    </div>
+    <input type="text" id="username" name="username" required><br>
+    </form>
+</body>
+</html>
